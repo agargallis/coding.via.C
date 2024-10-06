b6869d65ca9ce69df588d0d2927e986468851017
Antonis Gargallis <antonisgargallis@gmail.com>
2024-10-06 19:46:02 +0300

    Add files via upload
    //THE BEGGINING OF LEARNING CODING

---
+++ lec0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ (Α' ΕΞΑΜΗΝΟ) ΜΕ ΚΥΡΙΟ ΑΥΓΕΡΙΝΟ
+
+
+int main()
+{
+    printf ("Hello World of Informatics and Telecommunications\n");
+
+    return 0;
+}
+
