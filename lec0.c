4d01b317af0cce2470a7d9fda32f40fa8c357a77
Antonis Gargallis <antonisgargallis@gmail.com>
2024-10-13 19:13:32 +0300

    Update lec0.c

--- lec0.c
+++ lec0.c
@@ -3,9 +3,8 @@
 //ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ (Α' ΕΞΑΜΗΝΟ) 1
 
 
-int main(){
-    printf ("Hello World of Informatics and Telecommunications\n");
+int main(){ //MAIN FUNCTION OF THE PROGRAM - ΕΙΝΑΙ ΑΥΤΟΝΟΜΗ ΚΑΙ ΠΑΙΖΕΙ ΣΗΜΑΝΤΙΚΟ ΡΟΛΟ ΓΙΑ ΤΗΝ ΕΠΙΤΕΥΞΗ ΤΟΥ ΠΡΟΓΡΑΜΜΑΤΟΣ (NOTE)
+    printf ("Hello World of Informatics and Telecommunications"); //ΕΚΤΥΠΩΝΕΙ ΤΟ "..." ΚΑΙ Η PRINTF ΠΑΝΤΑ ΚΑΤΑΛΗΓΕΙ ΜΕ ; (ΝΟΤΕ)
 
-    return 0;
-}
-
+    return 0; //Η ΕΠΙΣΤΡΟΦΗ ΜΗΔΕΝ ΣΤΟΝ ΧΡΗΣΤΗ ΦΑΝΕΡΩΝΕΙ ΕΠΙΤΥΧΙΑ (ΝΟΤΕ)
+} //ΧΡΕΙΑΖΟΝΤΑΙ ΠΑΝΤΑ ΑΓΚΥΛΕΣ ΓΙΑ ΟΛΗ ΤΗΝ ΜΑΙΝ ΑΠΟ ΤΗΝ ΑΡΧΗ ΜΕΧΡΙ ΚΑΙ ΤΟ ΤΕΛΟΣ {} (ΝΟΤΕ)
