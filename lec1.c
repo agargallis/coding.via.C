8e564296abb0064512bc5a07a25b5927b52bfbe8
Antonis Gargallis <antonisgargallis@gmail.com>
2024-10-13 19:15:48 +0300

    Create lec1.c

---
+++ lec1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdiNT.h> //A (NOTE)
+
+
+
+//ΕΙΣΑΓΩΓΗ ΣΤΟΝ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟ (Α' ΕΞΑΜΗΝΟ) 2
+
+int main(){
+    
+    int x=3;
+    int y=0x2A; //ΔΕΚΑΕΞΑΔΙΚΟ ΣΥΣΤΗΜΑ (NOTE)
+    char z=42; //ΧΑΡΑΚΤΗΡΕΣ ASCΙΙ (ΝΟΤΕ)
+    unsigned long int w=10000000;
+    unsigned int i=-1;
+    uint64_t h=-2; //A - 64 BITS INTEGER(NOTE)
+
+    printf("Number 3 is presented in real math language as %d, but in hexadecimal system as %d!\nNumber 42 in ASCII mode is written also as %c !", x, y, z); 
+    printf("\nA huge unsigned and integer number is %ld", w);
+    printf("\n%u", i); //-1 IS SIGNED NUMBER (NOTE)
+    //ΜΠΟΡΩ ΝΑ ΚΑΝΩ ΠΡΑΞΕΙΣ ΚΑΙ ΜΕ ASCII (NOTE)
+    
+
+    //EXTRA SIGNIFICANT NOTES (NOTE)
+
+    
+    //char (-128 … 127) (NOTE)
+    //short int (-32.768 … 32.767) (NOTE)
+    //int (-2.147.483.648 … 2.147.483.647) (NOTE)
+    //long int (-2.147.483.648 … 2.147.483.647) (NOTE)
+    //float (-1.17*10-38 … 3.4*1038) (NOTE)
+    //double (-2.2*10-308 … 1.8*10308) (NOTE)
+    //long double (NOTE)
+    //unsigned char (0 … 255) (NOTE)
+    //unsigned short int (0 … 65535) (NOTE)
+    //unsigned int (0 … 4.294.967.295) (NOTE)
+    //unsigned long int (0 … 4.294.967.295) (NOTE)
+
+    //ME ΤΟ sizeof ΒΡΙΣΚΟΥΜΕ ΤΟ ΜΕΓΕΘΟΣ ΕΝΟΣ ΤΥΠΟΥ ΔΗΛΑΔΗ ΤΑ ΜΠΆΙΤΣ (NOTE)
+    //ΜΕ ΤΟ <stdint.h> ΒΑΖΩ ΑΚΕΡΑΙΟΥΣ ΜΠΟΡΟΥΜΕ ΝΑ ΔΗΛΩΣΟΥΜΕ ΑΚΕΡΑΙΟΥΣ ΜΕ ΤΗΝ ΕΠΙΘΥΜΗΤΗ ΑΚΡΙΒΕΙΑ ΟΠΩΣ int8_t (NOTE)
+
+
+    // \n: AΛΛΑΓΗ ΓΡΑΜΜΗΣ (NOTE)
+    // \r: ΕΠΑΝΑΦΟΡΑ ΤΟΥ ΚΕΡΣΟΡΑ ΣΤΗΝ ΑΡΧΗ ΤΗΣ ΤΡΕΧΟΥΣΑΣ ΓΡΑΜΜΗΣ (NOTE)
+    // \t: ΤΥΠΩΜΑ ΕΝΟΣ ΚΕΝΟΥ ΙΣΟ ΜΕ TAB (NOTE)
+    // \\: ΤΥΠΩΜΑ ΑΝΑΣΤΡΟΦΗΣ ΚΕΚΛΙΜΕΝΗΣ (BACKSLASH) (NOTE)
+    // \": ΤΥΠΩΜΑ ΔΙΠΛΩΝ ΕΙΣΑΓΩΓΙΚΩΝ (double quotes) (") (NOTE)
+    // \xNN: ΤΥΠΩΜΑ ΤΟΥ ΧΑΡΑΚΤΗΡΑ ΠΟΥ ΑΝΤΙΣΤΟΙΧΕΙ ΣΕ NN ΣΕ ΔΕΚΑΕΞΑΔΙΚΟ (NOTE)
+    // \a: ΔΗΜΙΟΥΡΓΙΑ ΗΧΗΤΙΚΟΥ ΣΗΜΑΤΟΣ (NOTE)
+
+    //FORMAT SPECIFIERS (NOTE)
+    //%c: ΤΥΠΩΜΑ ASCII ΧΑΡΑΚΤΗΡΑ (NOTE)
+    //%d ή %i: ΤΥΠΩΜΑ ΑΚΕΡΑΙΟΥ (NOTE)
+    //%u: ΤΥΠΩΜΑ UNSIGNED (ΜΗ ΠΡΟΣΗΜΑΣΜΕΝΟΥ) ΑΚΕΡΑΙΟΥ (NOTE)
+    //%f: ΤΥΠΩΜΑ FLOAT (NOTE)
+    //%llu: ΤYΠΩΜΑ LONG LONG UNSIGNED INT (NOTE)
+    //%%: ΤΥΠΩΜΑ ΤΟΥ ΧΑΡΑΚΤΗΡΑ % (NOTE)
+
+    //ΔΕΣΜΕΥΜΕΝΕΣ ΛΕΞΕΙΣ 1-> auto, break, case, char, const, continue, default, do, double, else, enum, extern, for, float, goto, while (NOTE)
+    //ΔΕΣΜΕΥΜΕΝΕΣ ΛΕΞΕΙΣ 2-> if, int, long, register, return, short, signed, sizeof, static, struct, switch, typedef, union, unsigned, void, volatile (NOTE)
+
+
+    return 0;
+}
